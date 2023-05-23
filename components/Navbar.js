fcd3e7576c505c82920ef0094b84a7d21b638316
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-05-23 22:51:00 +0900

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+import { 'Navbar' } from './Footer.module.css'
+import Link from 'next/link'
+
+export default function Footer() {
+  return (
+    <>
+      <footer className={styles.footer}>
+        <Link href="/"> 
+          <a><img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} /></a>
+        </Link>
+      </footer>
+    </>
+  )
+}
